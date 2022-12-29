1902e6f0b76f4b56e5abf7d1d778a098d6024032
ramos21rodrigo <ramos03rodrigo@gmail.com>
2022-12-29 18:44:57 +0000

    finish:125%

---
+++ client_bonus.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roramos <roramos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/27 23:15:14 by roramos           #+#    #+#             */
+/*   Updated: 2022/11/29 17:14:44 by roramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	args_check(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 3)
+		handle_errors("Invalid number of arguments");
+	while (argv[1][i])
+		if (!ft_isdigit(argv[1][i++]))
+			handle_errors("Invalid PID");
+	if (*argv[2] == 0)
+		handle_errors("Invalid message (empty)");
+}
+
+void	send_msg(pid_t sv_pid, char *msg)
+{
+	unsigned char	c;
+	int				bits;
+
+	while (*msg)
+	{
+		c = *msg++;
+		bits = 8;
+		while (bits--)
+		{
+			if (c & 0b10000000)
+				kill(sv_pid, SIGUSR1);
+			else
+				kill(sv_pid, SIGUSR2);
+			usleep(50);
+			c <<= 1;
+		}
+	}
+}
+
+void	sig_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		printf("Character send!\n");
+}
+
+void	create_signal(void)
+{
+	struct sigaction	sa_newsig;
+
+	sa_newsig.sa_handler = &sig_handler;
+	sa_newsig.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
+		handle_errors("Failed to change SIGUSR1's behavior");
+}
+
+int	main(int argc, char **argv)
+{
+	args_check(argc, argv);
+	create_signal();
+	send_msg(ft_atoi(argv[1]), argv[2]);
+	return (EXIT_SUCCESS);
+}
---
+++ server_bonus.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roramos <roramos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/27 23:15:39 by roramos           #+#    #+#             */
+/*   Updated: 2022/11/29 17:17:31 by roramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+void	handle_sigusr(int signum, siginfo_t *info, void *ucontent)
+{
+	static int				bits = -1;
+	static unsigned char	c;
+
+	(void)ucontent;
+	if (bits < 0)
+		bits = 7;
+	if (signum == SIGUSR1)
+		c |= (1 << bits);
+	bits--;
+	if (bits < 0 && c)
+	{
+		ft_putchar_fd(c, STDOUT_FILENO);
+		c = 0;
+		if (kill(info->si_pid, SIGUSR1) == -1)
+			handle_errors("Failed to send SIGUSR1");
+	}
+}
+
+void	create_signal(void)
+{
+	struct sigaction	sa_newsig;
+
+	sa_newsig.sa_sigaction = &handle_sigusr;
+	sa_newsig.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
+		handle_errors("Failed to change SIGUSR1's behavior");
+	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
+		handle_errors("Failed to change SIGUSR2's behavior");
+}
+
+int	main(void)
+{
+	ft_printf("Server pid: %d\n", getpid());
+	create_signal();
+	while (1)
+		pause();
+	return (EXIT_SUCCESS);
+}
